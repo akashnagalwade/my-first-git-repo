6633fb17870b91fc8a748f43b0ba2978f8341d23
akash <akashnagalwade444@gmail.com>
2024-01-09 22:57:50 +0530

    2nd commit

--- first.java
+++ first.java
@@ -2,5 +2,7 @@ public class first {
 
     public static void main(String[] args) {
         System.out.println("first project");
+                System.out.println("first project");
+
     }
-}
+} 
